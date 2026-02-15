2beb519bdd367e6923df5a3caf61bc3a682518bc
Mickael-Desclaux <mickaeldesclaux@gmail.com>
2026-02-15 13:57:58 +0100

    handle idle state and animation

--- Assets/Game/Player/Inputs/InputListener.cs
+++ Assets/Game/Player/Inputs/InputListener.cs
@@ -1,25 +1,24 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 namespace Game.Player
 {
     public class InputListener : MonoBehaviour
     {
-        public event Action<Vector2> Moved;
-        public event Action Jumped;
+        public UnityEvent<Vector2> Moved;
+        public UnityEvent Jumped;
 
         private void OnMove(InputValue value)
         {
             Vector2 move = value.Get<Vector2>();
-            Debug.LogWarning($"x: {move.x}, y: {move.y}");
             Moved?.Invoke(move);
         }
 
         private void OnJump()
         {
             Jumped?.Invoke();
-            Debug.LogWarning("Jump!");
         }
     }
 }
--- Assets/Game/Player/Scripts/States/IdleState.cs
+++ Assets/Game/Player/Scripts/States/IdleState.cs
@@ -1,9 +1,12 @@
+using UnityEngine;
 using Library.FSM;
 
 namespace Game.Player
 {
     public class IdleState : State<Player>
     {
+        private Vector2 _direction;
+
         public IdleState(Player context) : base(context)
         {
             
@@ -11,7 +14,9 @@ namespace Game.Player
 
         public override void Enter()
         {
-            
+            // Susbscribe to input events
+            Context.InputListener.Moved.AddListener(OnMoved);
+            Context.InputListener.Jumped.AddListener(OnJumped);
         }
 
         public override void Execute()
@@ -20,6 +25,18 @@ namespace Game.Player
         }
 
         public override void Exit()
+        {
+            // Remove listeners
+            Context.InputListener.Moved.RemoveListener(OnMoved);
+            Context.InputListener.Jumped.RemoveListener(OnJumped);
+        }
+
+        private void OnMoved(Vector2 direction)
+        {
+            _direction = direction;
+        }
+
+        private void OnJumped()
         {
             
         }
